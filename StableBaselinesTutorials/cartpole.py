fac5307fc8245f08834cab4f1c25248556cfec60
Landon Butler <landonb272@gmail.com>
2020-11-01 15:29:10 -0600

    Added a few more tutorials

--- StableBaselinesTutorials/cartpole.py
+++ StableBaselinesTutorials/cartpole.py
@@ -4,16 +4,21 @@ from stable_baselines.common.policies import MlpPolicy
 from stable_baselines.common.vec_env import DummyVecEnv
 from stable_baselines import PPO2
 
+# create environment in open ai gym
 env = gym.make('CartPole-v1')
-# Optional: PPO2 requires a vectorized environment to run
-# the env is now wrapped automatically when passing it to the constructor
-# env = DummyVecEnv([lambda: env])
 
+# model takes in environment, verbose is logger information, 
+# we will need to design our own policy, with the GNN as the actor
 model = PPO2(MlpPolicy, env, verbose=1)
+# train the model
 model.learn(total_timesteps=10000)
 
 obs = env.reset()
 for i in range(1000):
+    # based on an observation, figure out what the model will do. This will 
+    # be a loop over all agents for us
     action, _states = model.predict(obs)
+    # based on the action(s), see how the environment is updated
     obs, rewards, dones, info = env.step(action)
+    # show the outcome of the action
     env.render()
