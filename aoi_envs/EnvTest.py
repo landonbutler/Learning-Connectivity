69ba2a240ef647f76db4777436c16f9959ede22d
Landon Butler <landonb272@gmail.com>
2020-11-29 19:22:30 -0600

    Created MultiAgent superclass, mobile envrionment, and added renders

---
+++ aoi_envs/EnvTest.py
@@ -0,0 +1,41 @@
+import unittest
+import numpy as np
+from Stationary import StationaryEnv
+from Mobile import MobileEnv
+import imageio
+import unittest
+from graph_nets import utils_np
+import networkx as nx
+import matplotlib.pyplot as plt
+import random
+import gym
+import os
+
+class EnvTest(unittest.TestCase):
+    def setUp(self):
+        self.env = MobileEnv()
+        self.env.reset()
+
+    def test_controller(self):
+        tot_ts = 200
+        fp = "visuals/interference/mobile/"
+        cont = "Greedy" # Greedy, MST, or Random
+        for i in range(tot_ts):
+            if cont is "Greedy":
+                attempt_comm = self.env.greedy_controller()
+            elif cont is "MST":
+                attempt_comm = self.env.mst_controller()
+            else:
+                attempt_comm = self.env.random_controller()
+            observation, reward, done, info = self.env.step(attempt_comm)
+            self.env.render_interference(controller=cont, filepath=fp)
+        
+        with imageio.get_writer(fp + cont +'Interference' + str(self.env.n_agents) + '.gif', mode='I', duration=.05) as writer:
+            for i in range(1,tot_ts+1):
+                fileloc = fp + 'ts'+str(i)+'.png'
+                image = imageio.imread(fileloc)
+                writer.append_data(image)
+                os.remove(fileloc)
+
+if __name__ == '__main__':
+    unittest.main()
--- aoi_envs/__init__.py
+++ aoi_envs/__init__.py
@@ -1,8 +1,15 @@
 from aoi_envs.Stationary import StationaryEnv
+from aoi_envs.Mobile import MobileEnv
 from gym.envs.registration import register
 
 register(
     id='StationaryEnv-v0',
     entry_point='aoi_envs:StationaryEnv',
+    max_episode_steps=100,
+)
+
+register(
+    id='MobileEnv-v0',
+    entry_point='aoi_envs:MobileEnv',
     max_episode_steps=100,
 )
