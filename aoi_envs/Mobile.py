8d67721daba02907366abe369c7b17f4dffbd64f
Kate Tolstaya <kate.tolstaya@gmail.com>
2021-01-15 11:31:27 -0500

    more broadcasting for mobile agents and flocking

--- aoi_envs/Mobile.py
+++ aoi_envs/Mobile.py
@@ -18,12 +18,12 @@ class MobileEnv(MultiAgentEnv):
 
         self.flocking = flocking
         self.biased_velocities = biased_velocities
+        self.diag = np.eye(self.n_agents, dtype=np.bool).reshape(self.n_agents, self.n_agents, 1)
 
     def reset(self):
         super().reset()
         if self.flocking:
-            self.x[:, 2] = np.random.uniform(0.5 * -self.max_v, 0.5 * self.max_v, size=(self.n_agents,))
-            self.x[:, 3] = np.random.uniform(0.5 * -self.max_v, 0.5 * self.max_v, size=(self.n_agents,))
+            self.x[:, 2:4] = np.random.uniform(0.5 * -self.max_v, 0.5 * self.max_v, size=(self.n_agents, 2))
             if self.biased_velocities:
                 bias = np.random.uniform(0.5 * -self.max_v, 0.5 * self.max_v, size=(1, 2))
                 self.x[:, 2:4] = self.x[:, 2:4] + bias
@@ -32,10 +32,10 @@ class MobileEnv(MultiAgentEnv):
             self.x[:, 2] = self.max_v * np.cos(angle)
             self.x[:, 3] = self.max_v * np.sin(angle)
 
-        self.network_buffer[:, :, 4] = np.where(np.eye(self.n_agents, dtype=np.bool),
-                                                self.x[:, 2].reshape(self.n_agents, 1), self.network_buffer[:, :, 4])
-        self.network_buffer[:, :, 5] = np.where(np.eye(self.n_agents, dtype=np.bool),
-                                                self.x[:, 3].reshape(self.n_agents, 1), self.network_buffer[:, :, 5])
+        self.network_buffer[:, :, 4:6] = np.where(self.diag,
+                                                  self.x[:, 2:4].reshape(self.n_agents, 1, 2),
+                                                  self.network_buffer[:, :, 4:6])
+
         return self.get_relative_network_buffer_as_dict()
 
     def step(self, attempted_transmissions):
@@ -44,29 +44,16 @@ class MobileEnv(MultiAgentEnv):
 
     def move_agents(self):
         if self.flocking:
-            known_x_velocities = self.network_buffer[:, :, 4]
-            known_x_velocities[known_x_velocities == 0] = np.nan
-            self.x[:, 2] = np.nanmean(known_x_velocities, axis=1)
-
-            known_y_velocities = self.network_buffer[:, :, 5]
-            known_y_velocities[known_y_velocities == 0] = np.nan
-            self.x[:, 3] = np.nanmean(known_y_velocities, axis=1)
-
+            known_velocities = self.network_buffer[:, :, 4:6]
+            known_velocities[known_velocities == 0] = np.nan
+            self.x[:, 2:4] = np.nanmean(known_velocities, axis=1)
             self.x[:, 0:2] = self.x[:, 0:2] + self.x[:, 2:4] * self.ts_length
 
         else:
             new_pos = self.x[:, 0:2] + self.x[:, 2:4] * self.ts_length
-            self.x[:, 0] = np.clip(new_pos[:, 0], -self.r_max, self.r_max)
-            self.x[:, 1] = np.clip(new_pos[:, 1], -self.r_max, self.r_max)
+            self.x[:, 0:2] = np.clip(new_pos[:, 0:2], -self.r_max, self.r_max)
+            self.x[:, 2:4] = np.where((self.x[:, 0:2] - new_pos[:, 0:2]) == 0, self.x[:, 2:4], -self.x[:, 2:4])
 
-            self.x[:, 2] = np.where((self.x[:, 0] - new_pos[:, 0]) == 0, self.x[:, 2], -self.x[:, 2])
-            self.x[:, 3] = np.where((self.x[:, 1] - new_pos[:, 1]) == 0, self.x[:, 3], -self.x[:, 3])
-
-        self.network_buffer[:, :, 2] = np.where(np.eye(self.n_agents, dtype=np.bool),
-                                                self.x[:, 0].reshape(self.n_agents, 1), self.network_buffer[:, :, 2])
-        self.network_buffer[:, :, 3] = np.where(np.eye(self.n_agents, dtype=np.bool),
-                                                self.x[:, 1].reshape(self.n_agents, 1), self.network_buffer[:, :, 3])
-        self.network_buffer[:, :, 4] = np.where(np.eye(self.n_agents, dtype=np.bool),
-                                                self.x[:, 2].reshape(self.n_agents, 1), self.network_buffer[:, :, 4])
-        self.network_buffer[:, :, 5] = np.where(np.eye(self.n_agents, dtype=np.bool),
-                                                self.x[:, 3].reshape(self.n_agents, 1), self.network_buffer[:, :, 5])
+        self.network_buffer[:, :, 2:6] = np.where(self.diag,
+                                                  self.x[:, 0:4].reshape(self.n_agents, 1, 4),
+                                                  self.network_buffer[:, :, 2:6])
