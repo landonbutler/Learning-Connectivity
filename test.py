be3f7825e2491fb038001554c12e6769e37199fe
Kate Tolstaya <kate.tolstaya@gmail.com>
2021-01-16 11:57:51 -0500

    flocking reward scaled by 100

--- test.py
+++ test.py
@@ -133,7 +133,8 @@ if __name__ == '__main__':
         model_name = 'models/rl_Landon/EVE_GNN_5.pkl'
         model_name = 'models/nl11_2/ckpt/ckpt_180.pkl'
         model_name = 'models/nl12_4/ckpt/ckpt_040.pkl'
-        model_name = 'models/mobile12_10_2/ckpt/ckpt_004.pkl'
+
+        model_name = 'models/mobile12_10_2/ckpt/ckpt_040.pkl'
 
         # load the dictionary of parameters from file
         model_params, params = BaseRLModel._load_from_file(model_name)
@@ -157,7 +158,7 @@ if __name__ == '__main__':
         print('\nTest over 10  episodes live visualization...')
         eval_model(env, model, 10, render=True)
 
-    n_episodes = 10
+    n_episodes = 100
     print('\nTest over ' + str(n_episodes) + ' episodes...')
     results = eval_model(env, model, n_episodes)
     print('reward,          mean = {:.1f}, std = {:.1f}'.format(np.mean(results['reward']), np.std(results['reward'])))
